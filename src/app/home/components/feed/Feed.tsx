0b8a865fe4bedba4cb77b8db1f26275413a76ee9
Juan Carlos <ruluk@users.noreply.github.com>
2020-07-07 11:59:08 -0500

    Fix: Show nav only in home for now.

--- src/app/home/components/feed/Feed.tsx
+++ src/app/home/components/feed/Feed.tsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Feed extends Component {
   render() {
-    return <p>Feed</p>;
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/registration">Registration</Link>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
 
