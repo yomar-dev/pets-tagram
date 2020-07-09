5e3785d83b525a704cf2f1988beb724ec54f58f1
Yomar Miranda <mirandayomar@gmail.com>
2020-07-09 11:25:20 -0500

    feat: add value property to the input component.

--- src/app/shared/components/input/Input.tsx
+++ src/app/shared/components/input/Input.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './input.scss';
 
-export const Input = ({ type = 'text', placeholder = '' }) => {
-  return <input type={type} placeholder={placeholder} className="pets-input" />;
+export const Input = ({ type = 'text', value = '', placeholder = '' }) => {
+  return <input type={type} value={value} placeholder={placeholder} className="pets-input" />;
 };
 
 export default Input;
