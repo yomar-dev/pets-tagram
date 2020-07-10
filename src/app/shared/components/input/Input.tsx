0259a60398c2992114961b66a78b80a6735e104d
Yomar Miranda <mirandayomar@gmail.com>
2020-07-09 20:38:47 -0500

    feat: create inputs styles.

--- src/app/shared/components/input/Input.tsx
+++ src/app/shared/components/input/Input.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './input.scss';
 
 export const Input = ({ type = 'text', value = '', placeholder = '' }) => {
-  return <input type={type} value={value} placeholder={placeholder} className="pets-input" />;
+  return <input type={type} value={value} placeholder={placeholder} className="input" />;
 };
 
 export default Input;
